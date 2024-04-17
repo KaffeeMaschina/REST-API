7bf21416d9fe64d522f8d467f12a627d6be840df
KaffeeMaschina <nikitaki.789@gmail.com>
2024-04-17 12:55:46 +0300

    modified:   internal/nats-streaming/main.go

--- internal/nats-streaming/main.go
+++ internal/nats-streaming/main.go
@@ -1,8 +1,6 @@
 package main
 
-import "github.com/KaffeeMaschina/http-rest-api/internal/nats-streaming"
-
 func main() {
-	nats.Subscriber()
-	nats.Publisher()
+	main.Subscriber()
+	main.Publisher()
 }
